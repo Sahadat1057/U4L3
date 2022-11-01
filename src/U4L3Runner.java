1ae39ceb4685a5fceffeb2f0b9fcc6e8afd69148
sahadatamzad <samzad7698@bths.edu>
2022-10-31 21:13:45 -0400

    trying to do isPrime method

--- src/U4L3Runner.java
+++ src/U4L3Runner.java
@@ -1,10 +1,12 @@
 public class U4L3Runner {
          public static void main(String[] args) {
             WhileLoopFun fun = new WhileLoopFun();
-            System.out.println("-- test factorial --");
-            System.out.println(fun.factorial(6));
-            System.out.println(fun.factorial(10));
-            System.out.println(fun.factorial(20));
+             System.out.println(fun.isPrime(2));
+             System.out.println(fun.isPrime(13));
+             System.out.println(fun.isPrime(14));
+             System.out.println(fun.isPrime(30));
+             System.out.println(fun.isPrime(1));
 
-        }
+
+         }
     }
